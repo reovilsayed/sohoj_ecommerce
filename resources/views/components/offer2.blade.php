299bb42bbb9156c09fdd4fd9d7c60394914013fd
asalamin787 <asalaminsikder787@gmail.com>
2025-07-08 19:44:25 +0600

    done

--- resources/views/components/offer2.blade.php
+++ resources/views/components/offer2.blade.php
@@ -1,25 +1,25 @@
 <div class="row">
     <div class="col-lg-4 ps-0 d-flex mid-bn mb-4 me-5 margin-left img-fluid"
-        style="height: 275px; overflow: hidden; position: relative; background-size: cover;background-repeat: no-repeat; background-image: url('assets/img/1.jpg');">
+        style="height: 275px; overflow: hidden; position: relative; background-size: cover;background-repeat: no-repeat; background-image: url('assets/img/offer1.jpg');">
 
-        <div class="p-4 ms-4">
+        {{-- <div class="p-4 ms-4">
             <p style="font-size: 14px">test offer1category</p>
             <h4>test offer1Title</h4>
             <a class="mid-btn mt-4 btn btn-dark" href="#"><span
                     style="font-size: 10px">View Collections</span></a>
-        </div>
+        </div> --}}
     </div>
 
 
     <div class="col-lg-7 mid-bn mb-4 img-fluid"
-        style="height: 275px;overflow: hidden;background-size: cover;background-image: url('assets/img/1.jpg')">
+        style="height: 275px;overflow: hidden;background-size: cover;background-image: url('assets/img/9604469.jpg')">
 
-        <div class=" p-4 ms-4">
+        {{-- <div class=" p-4 ms-4">
             <p>test offer2category</p>
-            <h4>test offer2Title</h4>
+            <h4 class=" text-light">FASHION SALE</h4>
             <a class="mid-btn mt-4 btn btn-dark" href=""><span
                     style="font-size: 10px">View Collection</span></a>
-        </div>
+        </div> --}}
 
 
     </div>
