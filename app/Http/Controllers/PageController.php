bbadb0d37bcef073fc5e0b05abef85c8bf48d3e9
asalamin787 <asalaminsikder787@gmail.com>
2025-07-08 19:08:30 +0600

    done

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -38,7 +38,7 @@ class PageController extends Controller
 
         $bestsaleproducts = \Cache::remember('bestsaleproducts:' . md5(json_encode($locationPostcodes)), 3600, function () use ($locationPostcodes) {
             return Product::query()
-                ->select(['id','slug', 'name', 'shop_id', 'total_sale', 'post_code', 'status', 'parent_id'])
+                ->select(['id','slug', 'name', 'shop_id', 'total_sale', 'post_code', 'status', 'parent_id','image', 'images'])
                 ->where('status', 1)
                 ->whereNull('parent_id')
                 ->whereHas('shop', fn($q) => $q->where('status', 1))
