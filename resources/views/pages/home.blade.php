0f24a25854ce7476c6ee8f1564e2b8112f17ccbd
asalamin787 <asalaminsikder787@gmail.com>
2025-07-12 07:37:25 +0600

    done

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -669,7 +669,7 @@
             .category-slide {
                 flex: 0 0 calc(50% - 15px);
             }
-            
+
             .category-slider-container {
                 padding: 0 50px;
             }
@@ -711,7 +711,7 @@
     <div class="hero">
         <div class="container">
             <div class="row mt-4">
-                <div class="col-lg-3 ps-0">
+                <div class="col-lg-3 ps-md-0 d-none d-md-block">
                     <div class="hero__categories rounded-4 shadow-lg overflow-hidden bg-white border-0">
                         <!-- Toggle Header -->
                         <div class="hero__categories__all d-flex align-items-center justify-content-between px-4 py-4 bg-gradient-primary"
@@ -731,7 +731,7 @@
                         </div>
 
                         <!-- Category List -->
-                        <div id="static-category-list" style="display: block; max-height: 375px; overflow-y: auto;">
+                        <div id="static-category-list" style="display: block; max-height: 333px; overflow-y: auto;">
                             <div class="category-list-wrapper p-3">
                                 @foreach ($categories as $category)
                                     <div class="category-item mb-2">
@@ -767,7 +767,7 @@
                     </div>
                 </div>
 
-                <div class="col-lg-9 pe-0">
+                <div class="col-lg-9 pe-md-0">
                     <div class="hero-slider-wrapper">
                         <div class="hero-slider" role="region" aria-label="Product carousel">
                             <!-- Slide 1 -->
@@ -859,7 +859,7 @@
                     </div>
                 </div>
             </div>
-            
+
             <div class="category-slider-container">
                 <div class="category-slider-wrapper">
                     <div class="category-slider" id="categorySlider">
@@ -869,9 +869,8 @@
                                     <div class="category-card-inner">
                                         <div class="category-image-wrapper">
                                             <div class="category-image">
-                                                <img src="https://wpmayor.com/wp-content/uploads/2016/10/The-Beginners-Guide-To-WooCommerce-Product-Categories-Tags-Attributes-Banner-630x350.jpg" 
-                                                     alt="{{ $prodcat->name }}" 
-                                                     class="category-img">
+                                                <img src="https://wpmayor.com/wp-content/uploads/2016/10/The-Beginners-Guide-To-WooCommerce-Product-Categories-Tags-Attributes-Banner-630x350.jpg"
+                                                    alt="{{ $prodcat->name }}" class="category-img">
                                                 <div class="category-overlay">
                                                     <div class="category-icon">
                                                         <i class="fas fa-shopping-bag"></i>
@@ -889,8 +888,8 @@
                                                     {{ $prodcat->Products->count() ?? 0 }} products
                                                 </span>
                                             </div>
-                                            <a href="{{ route('shops', ['category' => $prodcat->slug]) }}" 
-                                               class="category-link">
+                                            <a href="{{ route('shops', ['category' => $prodcat->slug]) }}"
+                                                class="category-link">
                                                 <span>Explore Category</span>
                                                 <div class="link-icon">
                                                     <i class="fas fa-arrow-right"></i>
@@ -902,9 +901,9 @@
                             </div>
                         @endforeach
                     </div>
-                    
 
-                    
+
+
 
                 </div>
             </div>
@@ -1013,9 +1012,6 @@
                                                                     <x-products.product-1 :product="$product" />
                                                                 @endforeach
 
-
-
-
                                                             </div>
 
                                                         </div>
@@ -1025,21 +1021,15 @@
 
                                         </div>
                                     </div>
-
-
-
                                 </div>
                             </div>
                             <!-- New Product Content -->
-
                         </div>
                     </section>
                 @endif
             </div>
         </div>
-
     </section>
-    <div class="container border-bottom mb-5"></div>
     <!-- ec Product tab Area End -->
     <!-- Explore shop -->
     @if ($latest_shops->count() > 0)
@@ -1095,10 +1085,10 @@
                         <!-- Product tab area start -->
                         <div class="row space-t-50">
                             <!-- <div class="col-md-12">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        <div class="section-title">
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            <h2 class="ec-title">New Products</h2>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </div>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    </div> -->
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                <div class="section-title">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    <h2 class="ec-title">New Products</h2>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </div>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            </div> -->
                             <h2 class="related-product-sec-title my-5"> Recommended For You</h2>
                         </div>
 
@@ -1111,7 +1101,7 @@
                                             <div class="col-md-3">
                                                 <x-shops-card.card-1 :shop="$shop" />
                                             </div>
-                                            <div class="col-md-9">
+                                            <div class="col-md-9 mt-4">
                                                 <div class="ec-spe-products">
                                                     @foreach ($shop->products->whereNull('parent_id')->chunk(3) as $products)
                                                         {{-- @dd($products) --}}
@@ -1168,9 +1158,7 @@
     @endif
     <!-- ec Product tab Area End -->
 
-    </div>
-    </div>
-    </div>
+
     <!-- Product tab area end -->
 @endsection
 @section('js')
@@ -1192,11 +1180,11 @@
         document.addEventListener('DOMContentLoaded', function() {
             const slider = document.getElementById('categorySlider');
             const slides = document.querySelectorAll('.category-slide');
-            
+
             let currentSlide = 0;
             const slidesPerView = getSlidesPerView();
             const maxSlides = slides.length - slidesPerView;
-            
+
             function getSlidesPerView() {
                 if (window.innerWidth <= 576) return 1;
                 if (window.innerWidth <= 768) return 1;
@@ -1204,31 +1192,31 @@
                 if (window.innerWidth <= 1200) return 3;
                 return 4;
             }
-            
+
             function updateSlider() {
                 const slideWidth = slides[0].offsetWidth + 30; // 30px gap
                 const translateX = -currentSlide * slideWidth;
                 slider.style.transform = `translateX(${translateX}px)`;
             }
-            
 
-            
+
+
             function nextSlide() {
                 if (currentSlide < maxSlides) {
                     currentSlide++;
                     updateSlider();
                 }
             }
-            
+
             function prevSlide() {
                 if (currentSlide > 0) {
                     currentSlide--;
                     updateSlider();
                 }
             }
-            
 
-            
+
+
             // Keyboard navigation
             document.addEventListener('keydown', function(e) {
                 if (e.key === 'ArrowLeft') {
@@ -1237,24 +1225,24 @@
                     nextSlide();
                 }
             });
-            
+
             // Touch/swipe support
             let startX = 0;
             let endX = 0;
-            
+
             slider.addEventListener('touchstart', function(e) {
                 startX = e.touches[0].clientX;
             });
-            
+
             slider.addEventListener('touchend', function(e) {
                 endX = e.changedTouches[0].clientX;
                 handleSwipe();
             });
-            
+
             function handleSwipe() {
                 const swipeThreshold = 50;
                 const diff = startX - endX;
-                
+
                 if (Math.abs(diff) > swipeThreshold) {
                     if (diff > 0) {
                         nextSlide();
@@ -1263,7 +1251,7 @@
                     }
                 }
             }
-            
+
             // Resize handler
             window.addEventListener('resize', function() {
                 const newSlidesPerView = getSlidesPerView();
@@ -1272,7 +1260,7 @@
                     updateSlider();
                 }
             });
-            
+
             // Initialize slider
             updateSlider();
         });
