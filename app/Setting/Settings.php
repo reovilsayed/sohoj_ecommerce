ab0a07823160115fc196bdaf0626c5047f313e39
asalamin787 <asalaminsikder787@gmail.com>
2025-07-28 20:04:24 +0600

    done

---
+++ app/Setting/Settings.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Setting;
+
+use App\Models\Setting;
+use Illuminate\Support\Facades\Storage;
+
+class Settings
+{
+    public static function setting ($key, $default = null)
+    {
+        $setting = Setting::where('key', $key)->first();
+        
+        if($setting && $setting->type == 'file'){
+           return Storage::url($setting->data);
+        }else{
+            return $setting->data ?? '';
+        }
+    }
+
+}
