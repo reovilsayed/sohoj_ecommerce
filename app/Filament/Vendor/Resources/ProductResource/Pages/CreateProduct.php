139ec71941e2ce0bc5a5804d7f7b60f33e975650
asalamin787 <asalaminsikder787@gmail.com>
2025-09-18 22:11:45 +0600

    done

--- app/Filament/Vendor/Resources/ProductResource/Pages/CreateProduct.php
+++ app/Filament/Vendor/Resources/ProductResource/Pages/CreateProduct.php
@@ -3,10 +3,14 @@
 namespace App\Filament\Vendor\Resources\ProductResource\Pages;
 
 use App\Filament\Vendor\Resources\ProductResource;
+use App\Setting\Settings;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Notifications\Notification;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+
 
 class CreateProduct extends CreateRecord
 {
@@ -34,5 +38,17 @@ class CreateProduct extends CreateRecord
         }
 
         return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $product = $this->record;
+        // Send email notification to admin
+        try {
+            Mail::to(Settings::setting(key: 'admin_email'))->send(new \App\Mail\ProductCreatedAdminMail($product));
+        } catch (\Exception $e) {
+            // Log error or handle gracefully
+            Log::error('Failed to send product creation email: ' . $e->getMessage());
+        }
     }
 }
